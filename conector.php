e4fb10fabb933acf50cccf24a2b9a1b8d336baf5
guillermomartinez <guillermomartinez@outlook.com>
2015-03-13 16:49:04 -0500

    clean

--- conector.php
+++ conector.php
@@ -2,12 +2,8 @@
 include("vendor/autoload.php");
 use GuillermoMartinez\Filemanager\Filemanager;
 
-//$extra = array("path"=>"userfiles/demo/");
-// $extra = array("debug"=>true,"separator" => "PqbFilemanager/userfiles","upload"=>array("size_max" => 2));
-$extra = array("debug"=>false,"separator" => "PqbFilemanager/public/userfiles","upload"=>array("size_max" => 10));
-// $extra = array("doc_root"=>"/home/demo","debug"=>true,"separator" => "/userfiles","upload"=>array("size_max" => 2));
+$extra = array("debug"=>false,"separator" => "public/userfiles","upload"=>array("size_max" => 10));
 // $extra = array();
 $f = new Filemanager($extra);
 $f->run();
-
 ?>
