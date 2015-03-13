46fec56050b272cb4ae42a5e34e218956a5f53c2
Guillermo <guillermomartinez@outlook.com>
2015-03-13 18:44:19 -0500

    Update conector.php

--- conector.php
+++ conector.php
@@ -2,6 +2,8 @@
 include("vendor/autoload.php");
 use GuillermoMartinez\Filemanager\Filemanager;
 
+// agrega tu propio metodo de autenticacion
+
 $extra = array("separator" => "filemanager/userfiles"));
 $f = new Filemanager($extra);
 $f->run();
